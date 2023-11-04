3784dcebc3cb1c7eba02ab336d8597aa15a7b6fe
merveolccay <148968203+merveolccay@users.noreply.github.com>
2023-11-04 16:21:25 +0300

    Add files via upload

---
+++ metotUsAlma.java
@@ -0,0 +1,24 @@
+package java101;
+import java.util.Scanner;
+
+public class metotUsAlma {
+    public static void usAlma(){
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Üssü hesaplanacak sayıyı giriniz: ");
+        int n= inp.nextInt();
+        System.out.print("Üs olacak sayıyı giriniz: ");
+        int r= inp.nextInt();
+        int total = 1;
+
+        for(int i = 1 ; i <= r ; i++){
+            total *= n;
+        }
+        System.out.println(n+"^"+r+ ":"+total);
+    }
+
+
+
+    public static void main(String[] args) {
+        usAlma();
+    }
+}
